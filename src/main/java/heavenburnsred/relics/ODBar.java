d7793434ad945e7ab69cfe2f8f430844efc82b5c
Junduo Zheng <1253314323@qq.com>
2025-09-27 01:20:52 +0800

    OD-all_enemy
    this.counter = this.counter + monstercounts * card.magicNumber

--- src/main/java/heavenburnsred/relics/ODBar.java
+++ src/main/java/heavenburnsred/relics/ODBar.java
@@ -14,16 +14,18 @@ import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import heavenburnsred.cards.Defend;
 import heavenburnsred.patches.HBRRelicClick;
 
-
 import java.util.Objects;
 
 import static heavenburnsred.BasicMod.makeID;
 
-public class ODBar extends HBRRelicClick{
-    private static final String NAME = "ODBar"; //The name will be used for determining the image file as well as the ID.
-    public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
-    private static final AbstractRelic.RelicTier RARITY = AbstractRelic.RelicTier.COMMON; //The relic's rarity.
-    private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; //The sound played when the relic is clicked.
+public class ODBar extends HBRRelicClick {
+    private static final String NAME = "ODBar"; // The name will be used for determining the image file as well as the
+                                                // ID.
+    public static final String ID = makeID(NAME); // This adds the mod's prefix to the relic ID, resulting in
+                                                  // modID:MyRelic
+    private static final AbstractRelic.RelicTier RARITY = AbstractRelic.RelicTier.COMMON; // The relic's rarity.
+    private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; // The sound played when
+                                                                                              // the relic is clicked.
 
     private static final int HITCOUNT = 120;
     private int handcards = 0;
@@ -38,20 +40,19 @@ public class ODBar extends HBRRelicClick{
         if (card.type == AbstractCard.CardType.ATTACK) {
             if (card.target == AbstractCard.CardTarget.ENEMY) {
                 this.counter = this.counter + card.magicNumber;
-            }
-            else if (card.target == AbstractCard.CardTarget.ALL_ENEMY) {
+            } else if (card.target == AbstractCard.CardTarget.ALL_ENEMY) {
                 for (AbstractMonster mon : (AbstractDungeon.getMonsters()).monsters) {
                     if (!mon.isDeadOrEscaped())
                         monstercounts++;
                 }
-                this.counter = this.counter + monstercounts;
+                this.counter = this.counter + monstercounts * card.magicNumber;
                 monstercounts = 0;
             }
-            //else if (Objects.equals(card.cardID, "heavenburnsred:Defend")){
-            //    this.counter = this.counter + 2;
-            //}
+            // else if (Objects.equals(card.cardID, "heavenburnsred:Defend")){
+            // this.counter = this.counter + 2;
+            // }
 
-            if (this.counter > HITCOUNT){
+            if (this.counter > HITCOUNT) {
                 this.counter = HITCOUNT;
             }
         }
@@ -59,16 +60,16 @@ public class ODBar extends HBRRelicClick{
 
     public void onRightClick() {
         AbstractPlayer p = AbstractDungeon.player;
-        if (!this.usedUp && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && this.counter >= 40) {
+        if (!this.usedUp && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT
+                && this.counter >= 40) {
             this.counter = this.counter - 40;
             handcards = AbstractDungeon.player.hand.size();
-            addToBot(new DiscardAction(p,p,handcards,true));
-            addToBot(new DrawCardAction(p,5));
+            addToBot(new DiscardAction(p, p, handcards, true));
+            addToBot(new DrawCardAction(p, 5));
             addToBot(new GainEnergyAction(3));
         }
 
     }
-
 
     @Override
     public String getUpdatedDescription() {
