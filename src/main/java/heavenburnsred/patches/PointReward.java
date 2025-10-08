30a123500a56186e429263654313b36c72324f22
Junduo Zheng <1253314323@qq.com>
2025-10-08 09:16:41 +0800

    pointRewardScreen update

--- src/main/java/heavenburnsred/patches/PointReward.java
+++ src/main/java/heavenburnsred/patches/PointReward.java
@@ -1,9 +1,12 @@
 package heavenburnsred.patches;
 
 
+import basemod.ReflectionHacks;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
+import com.megacrit.cardcrawl.ui.buttons.PeekButton;
 import heavenburnsred.cards.PointCard.LLcard;
 import heavenburnsred.cards.PointCard.LQcard;
 import heavenburnsred.cards.PointCard.TJcard;
@@ -27,13 +30,18 @@ public class PointReward extends RewardItem {
     }
 
     @Override
-    // 选择时展开没有跳过选项的4选1面板，参照许愿
+    // 选择时展开属性4选1面板
     public boolean claimReward() {
-        // 这里没有跳过按钮，还是应该修改一下
-        AbstractDungeon.cardRewardScreen.chooseOneOpen(this.cards);
-        // 返回时回到奖励页面
+        // 可以跳过，没有颂钵，没有peek按钮，选择后不进入牌组而是直接生效，选择后奖励消失
+        AbstractDungeon.cardRewardScreen.customCombatOpen(this.cards, "请选择要升级的属性", true);
+        PeekButton peekButton = ReflectionHacks.getPrivate(AbstractDungeon.cardRewardScreen, CardRewardScreen.class, "peekButton");
+        peekButton.hideInstantly();
+        ReflectionHacks.setPrivate(AbstractDungeon.cardRewardScreen, CardRewardScreen.class, "rItem", this);
+        ReflectionHacks.setPrivate(AbstractDungeon.cardRewardScreen, CardRewardScreen.class, "chooseOne", true);
+        ReflectionHacks.setPrivate(AbstractDungeon.cardRewardScreen, CardRewardScreen.class, "discovery", false);
+        // 跳过返回时回到奖励页面
         AbstractDungeon.previousScreen = AbstractDungeon.CurrentScreen.COMBAT_REWARD;
-        // 选择后该奖励消失
-        return true;
+        // 跳过后该奖励不消失
+        return false;
     }
 }
