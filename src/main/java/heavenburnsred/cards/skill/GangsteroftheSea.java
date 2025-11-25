7119d52c955cb67b13876dfc8039c19f06a5c541
bst86682165 <2200012256@stu.pku.edu.cn>
2025-11-25 23:03:46 +0800

    鱼

---
+++ src/main/java/heavenburnsred/cards/skill/GangsteroftheSea.java
@@ -0,0 +1,51 @@
+package heavenburnsred.cards.skill;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import heavenburnsred.actions.ChangeODBarHitAction;
+import heavenburnsred.cards.BaseCard;
+import heavenburnsred.character.MyCharacter;
+import heavenburnsred.powers.DefendDown;
+import heavenburnsred.util.CardStats;
+
+public class GangsteroftheSea extends BaseCard {
+    public static final String ID = makeID(GangsteroftheSea.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    public static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2);//The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int MAGIC = 1;
+    private static final int UPG_MAGIC = 1;
+    private static final int HITamount = 26;
+
+    public GangsteroftheSea() {
+        super(ID,info); //Pass the required information to the BaseCard constructor.
+        setMagic(MAGIC,UPG_MAGIC);
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPG_MAGIC);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(m,p,new DefendDown(m, 1, magicNumber)));
+        addToBot(new ChangeODBarHitAction(HITamount));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new GangsteroftheSea();
+    }
+}
