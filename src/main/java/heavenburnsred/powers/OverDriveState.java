4c34e2f49f971872d983080d4d35c33ae9f56de1
Junduo Zheng <1253314323@qq.com>
2025-12-03 23:16:23 +0800

    Update OverDriveState.java

--- src/main/java/heavenburnsred/powers/OverDriveState.java
+++ src/main/java/heavenburnsred/powers/OverDriveState.java
@@ -1,5 +1,8 @@
 package heavenburnsred.powers;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 
 import static heavenburnsred.BasicMod.makeID;
@@ -10,6 +13,11 @@ public class OverDriveState extends BasePower {
 
     public OverDriveState(AbstractCreature owner, int amount) {
         super(POWER_ID, TYPE, false, owner, amount);
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
     }
 
     public void updateDescription() {
