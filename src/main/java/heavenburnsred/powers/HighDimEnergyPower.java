4a3e17fe351c026daf984d71d9d1b39478c93158
Junduo Zheng <1253314323@qq.com>
2025-12-04 17:19:52 +0800

    HighDimEnergy
    高维再生能源

---
+++ src/main/java/heavenburnsred/powers/HighDimEnergyPower.java
@@ -0,0 +1,29 @@
+package heavenburnsred.powers;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static heavenburnsred.BasicMod.makeID;
+
+public class HighDimEnergyPower extends BasePower {
+    public static final String POWER_ID = makeID(HighDimEnergyPower.class.getSimpleName());
+    private static final PowerType TYPE = PowerType.BUFF;
+
+    public HighDimEnergyPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, TYPE, false, owner, amount);
+    }
+
+    @Override
+    public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if ((power.type == PowerType.DEBUFF && !power.ID.equals("Shackled") && source == this.owner && target != this.owner && !target.hasPower("Artifact")) ||
+            (power.type == PowerType.BUFF && source == this.owner && target == this.owner)) {
+            this.flash();
+            addToBot(new GainBlockAction(this.owner, this.owner, this.amount));
+        }
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+}
