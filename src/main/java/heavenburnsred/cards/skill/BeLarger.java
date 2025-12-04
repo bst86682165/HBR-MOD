66dd8f8ab34595c30a6c94654e62293a216ee3b0
Junduo Zheng <1253314323@qq.com>
2025-12-04 11:42:35 +0800

    BeLarger
    变大吧！

---
+++ src/main/java/heavenburnsred/cards/skill/BeLarger.java
@@ -0,0 +1,49 @@
+package heavenburnsred.cards.skill;
+
+import com.evacipated.cardcrawl.mod.stslib.actions.common.StunMonsterAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import heavenburnsred.actions.ApplyHBRStackPowerAction;
+import heavenburnsred.cards.BaseCard;
+import heavenburnsred.character.MyCharacter;
+import heavenburnsred.powers.DefendDown;
+import heavenburnsred.util.CardStats;
+
+public class BeLarger extends BaseCard {
+    public static final String ID = makeID(BeLarger.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    public static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2);//The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int STUN_TURN = 1;
+
+    public BeLarger() {
+        super(ID,info); //Pass the required information to the BaseCard constructor.
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() {
+        super.upgrade();
+        this.target = CardTarget.ALL_ENEMY;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!upgraded) {
+            addToBot(new StunMonsterAction(m, p, STUN_TURN));
+        }
+        else {
+            for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+                addToBot(new StunMonsterAction(mo, p, STUN_TURN));
+            }
+        }
+    }
+}
