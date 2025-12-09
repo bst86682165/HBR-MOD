179c33b724fe0418dab49a2317ce796a2211eba5
Junduo Zheng <1253314323@qq.com>
2025-12-09 16:59:22 +0800

    Colourful
    五彩缤纷

---
+++ src/main/java/heavenburnsred/cards/skill/ColourfulBlue.java
@@ -0,0 +1,44 @@
+package heavenburnsred.cards.skill;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+import heavenburnsred.cards.BaseCard;
+import heavenburnsred.character.MyCharacter;
+import heavenburnsred.util.CardStats;
+
+public class ColourfulBlue extends BaseCard {
+    public static final String ID = makeID(ColourfulBlue.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    public static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.SPECIAL, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            -2);//The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int MAGIC = 1;
+
+    public ColourfulBlue() {
+        super(ID,info); //Pass the required information to the BaseCard constructor.
+
+        setMagic(MAGIC);
+    }
+
+    @Override
+    public void onChoseThisOption() {
+        AbstractPlayer p = AbstractDungeon.player;
+        for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters)
+            addToBot(new ApplyPowerAction(mo, p, new WeakPower(mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        onChoseThisOption();
+    }
+}
---
+++ src/main/java/heavenburnsred/cards/skill/ColourfulWhite.java
@@ -0,0 +1,39 @@
+package heavenburnsred.cards.skill;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import heavenburnsred.cards.BaseCard;
+import heavenburnsred.character.MyCharacter;
+import heavenburnsred.util.CardStats;
+
+public class ColourfulWhite extends BaseCard {
+    public static final String ID = makeID(ColourfulWhite.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    public static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.SPECIAL, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            -2);//The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int MAGIC = 3;
+
+    public ColourfulWhite() {
+        super(ID,info); //Pass the required information to the BaseCard constructor.
+
+        setMagic(MAGIC);
+    }
+
+    @Override
+    public void onChoseThisOption() {
+        addToBot(new DrawCardAction(magicNumber));
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        onChoseThisOption();
+    }
+}
